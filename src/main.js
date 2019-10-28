5a65cb3cddbe536cfeff40f7317810a4eb849b07
REN XIANG <Seeumt@cumt.edu.cn>
2019-10-29 00:09:59 +0800

    "30"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: a => a(App)
+}).$mount('#app')//把这个挂载到 index.html中的 id=app的div 上去
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "../views/Login";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    }
+
+    //  delete {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')//动态导入 等用的时候再导入
+    // }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
